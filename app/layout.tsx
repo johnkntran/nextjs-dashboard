c0d7d40aa155a1bca8bea207e15d7f86ef15d312
John Tran <john.kn.tran@gmail.com>
2023-11-19 21:39:51 +0000

    Create layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import React from 'react';
 
-export default function RootLayout({
-  children,
-}: {
+interface RootLayoutProps {
   children: React.ReactNode;
-}) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
